ea7557f32f9e86f4ec789172f2e0ed54a57fd29c
Pranav-learner <pranavgupta32006@gmail.com>
2025-05-17 16:05:21 +0530

    Setup Appwrite , auth database, bucket(storeage) , also made conf folder so that all the environment stuff kept in string , so that later on it won't give any error

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL), // by doing this we will use appwriteURl , and it will always give a string value,
+    // this conf stuff is done , because the environement wants everything to be in Strings
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
