6eedff502f9ca7f9dc01e5a6ade404c79e08c81b
Pranav-learner <pranavgupta32006@gmail.com>
2025-05-17 15:36:09 +0530

    Learnt how to setup environment using vite not react , and how to access it  in vite , not react environment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_API_URL); // using vite
   return (
+    // using react we write process.env
     <>
       <p>Hello</p>
     </>
